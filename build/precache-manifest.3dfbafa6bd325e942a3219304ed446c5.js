c276bbac9782ce60f1143030a20ff1ada519db54
Sarah Maples <sarah.e.maples@gmail.com>
2019-05-06 14:23:10 -0500

    simplified cards and added my imgs

---
+++ build/precache-manifest.3dfbafa6bd325e942a3219304ed446c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589f82e176943e270a43a65e497b2240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5b4bad295c0b39b036",
+    "url": "/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7f5b4bad295c0b39b036",
+    "url": "/static/js/main.761c0960.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
